6d8cc696d804cf32216f345c9196cb0903cf8991
Nopennn <77796980+Nopennn@users.noreply.github.com>
2021-11-11 22:49:00 +0300

    Update figure.h

--- lab1/figure.h
+++ lab1/figure.h
@@ -1,13 +1,15 @@
 #ifndef FIGURE_H
 #define FIGURE_H
 
-#include <ostream>
+#include <iostream>
+#include "point.h"
 
 class Figure {
 public:
-  virtual void Print(std::ostream& os) = 0;
+  virtual size_t VertexesNumber() = 0;
   virtual double Area() = 0;
-  virtual ~Figure() {};
+  virtual void Print(std::ostream &os) = 0;
+
 };
 
 #endif // FIGURE_H
