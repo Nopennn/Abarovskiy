4f51be192821a26cec8057e34df93b262857e91c
Nopennn <77796980+Nopennn@users.noreply.github.com>
2021-11-13 18:30:52 +0300

    Update triangle.cpp

--- lab1/triangle.cpp
+++ lab1/triangle.cpp
@@ -1,37 +1,30 @@
 #include "triangle.h"
+#include <math.h>
 
-#include <iostream>
-#include <cmath>
+Triangle::Triangle() : a_(0, 0), b_(0, 0), c_(0, 0) {}
 
-Triangle::Triangle()
-    : side_a(0.0), side_b(0.0), side_c(0.0) {}
-
-Triangle::Triangle(double a, double b, double c)
-    : side_a(a), side_b(b), side_c(c) {}
+Triangle::Triangle(const Triangle &triangle) {
+  this->a_ = triangle.a_;
+  this->b_ = triangle.b_;
+  this->c_ = triangle.c_;
+}
 
 Triangle::Triangle(std::istream &is) {
-  is >> side_a >> side_b >> side_c;
+  std::cin >> a_ >> b_ >> c_;
 }
 
-Triangle::Triangle(const Triangle& other)
-    : Triangle(other.side_a, other.side_b, other.side_c) {}
-
-double Triangle::Area() {
-  double p = (side_a + side_b + side_c) / 2.0;
-  return std::sqrt(p * (p - side_a) * (p - side_b) * (p - side_c));
-}
-
-void Triangle::Print(std::ostream& os) {
-  os << "a = " << side_a << ", ";
-  os << "b = " << side_b << ", ";
-  os << "c = " << side_c << std::endl;
-}
-
-size_t Triangle::VertexesNumber()
-{
+size_t Triangle::VertexesNumber() {
   return 3;
 }
 
-Triangle::~Triangle() {
-  std::cout << "Triangle deleted" << std::endl;
+double Triangle::Area() {
+  double a = a_.dist(b_);
+  double b = b_.dist(c_);
+  double c = c_.dist(a_);
+  double p = (a + b + c) / 2;
+  return sqrt(p * (p - a) * (p - b) * (p - c));
+}
+
+void Triangle::Print(std::ostream &os) {
+  std::cout << "Triangle " << a_ << b_ << c_ << std::endl;
 }
