0a687be0548c383339285458396569c2bf9c4f95
Nopennn <77796980+Nopennn@users.noreply.github.com>
2022-01-20 04:57:43 +0300

    Add files via upload

---
+++ lab6/tvector.h
@@ -0,0 +1,36 @@
+//TVECTOR.H
+#ifndef TVECTOR_H
+#define TVECTOR_H
+
+#include <iostream>
+#include "triangle.h"
+#include "rectangle.h"
+#include "square.h"
+#include <memory>
+
+template <class T>
+
+class TVector
+{
+	private:
+		void resize(int newsize);
+		std::shared_ptr<T> *vals;
+		int len;
+		int rLen;
+	public:
+		TVector();
+		TVector(const TVector<T>& other);
+		void Erase(int pos);
+		void InsertLast(const std::shared_ptr<T> t);
+		void RemoveLast();
+		const std::shared_ptr<T>& Last();
+		std::shared_ptr<T>& operator[](const size_t idx);
+		bool Empty();
+		size_t Length();
+		template<typename Y>
+  		friend std::istream &operator>>(std::istream &is, TVector<Y> &object);
+		void Clear();
+		~TVector();
+};
+
+#endif
